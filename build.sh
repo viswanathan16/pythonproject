05185d5f87f39156b3fba8f67f0dd60f6e7093b2
Cristiano1407 <sirius16797@gmail.com>
2025-01-03 11:17:44 +0000

    new build

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 set -e
+docker ps -a
+
+if [ "$(docker ps -a)" ]; then
+	echo "stopping all containers"
+	
+	docker stop $(docker ps -aq)
+	docker rm $(docker ps -aq)
+else 
+	echo "no containers are running...."
+fi
+
 docker build -t viswanathan1407/flask-app:v2 .
 docker run -itd -p 5000:5000 viswanathan1407/flask-app:v2
 
